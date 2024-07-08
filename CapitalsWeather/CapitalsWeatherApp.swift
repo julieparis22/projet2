63bfa35e283928e6a12e708876d26f354a5f2900
julie <julie.ryan.france@gmail.com>
2024-07-08 16:27:38 +0200

    Initial Commit

---
+++ CapitalsWeather/CapitalsWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalsWeatherApp.swift
+//  CapitalsWeather
+//
+//  Created by julie ryan on 08/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CapitalsWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
